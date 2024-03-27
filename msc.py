a8189066d7864155a17c0cf5e5c0d4650899eab4
metetas <120932626+metetas@users.noreply.github.com>
2024-03-27 16:50:42 +0300

    Add files via upload

---
+++ msc.py
@@ -0,0 +1,29 @@
+def tplm():
+    s1=int(input("#input= "))
+    s2=int(input("#input= "))
+    print(s1+s2)
+def ckrm():
+    s1=int(input("#input= "))
+    s2=int(input("#input= "))
+    print(s1-s2)
+def crpm():
+    s1=int(input("#input= "))
+    s2=int(input("#input= "))
+    print(s1*s2)
+def blm():
+    s1=int(input("#input= "))
+    s2=int(input("#input= "))
+    print(s1/s2)
+def tkcft():
+    s=int(input("#input= "))
+    k=s%2
+    if k==0:
+        print("çift")
+    else:
+        print("tek")
+def kln():
+    s=int(input("#input= "))
+    b=int(input("#input= "))
+    k=s%b
+    print(k)
+
